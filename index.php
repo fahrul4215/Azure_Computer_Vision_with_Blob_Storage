1f94a9034cd0cfc5a47d900eada3b15f06464e12
fahrul4215 <fahrul4215@gmail.com>
2019-07-09 23:09:35 +0700

    lol to index

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
         $linkBlob = "https://fahrul4215dicoding.blob.core.windows.net/".$_SESSION['nama_container']."/".$_SESSION['nama_file'];
     }
 
-    require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-    require_once $_SERVER['DOCUMENT_ROOT']."random_string.php";
+    define('__ROOT__', dirname(__FILE__));
+    require_once __ROOT__.'/vendor/autoload.php';
+    require_once __ROOT__."/random_string.php";
 
     use MicrosoftAzure\Storage\Blob\BlobRestProxy;
     use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
